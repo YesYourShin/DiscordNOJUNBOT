023aca02f7d4ce677314235e897e55e1a43fc1ff
YesYourShin <ehrl827@naver.com>
2021-06-26 09:28:37 +0900

    add keyword selection

--- anywords.js
+++ anywords.js
@@ -1,6 +1,21 @@
 function anyWords(dictionary, keyword) {
     const result = [];
+    const match = [];
+    
+    
+
+    const keys = Object.keys(dictionary);
+    for(let key of keys) {
+        if (key.startsWith(keyword)) {
+            match.push(key); 
+        }
+        
+    }
+
+    const krd = Math.floor(Math.random() * match.length);
+    keyword = match[krd];
     result.push(keyword);
+    
     for(let i = 0; i < Math.floor(Math.random() * 10) + 10 ; i++) {
         const talk = dictionary[keyword];
         if(!talk) break; 
